8d220ed7289677d69ca1477bf873ba6a9d82eceb
Felipe Gonzalez <adonaac@gmail.com>
2012-11-03 01:55:00 -0200

    Testing testing

--- decline/main.py
+++ decline/main.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-import string
+import string, os
 
 def main():
-    f = open('/home/adn/decline/tsg.txt', 'r')
+    homedir = os.path.expanduser('~')
+    f = open(os.path.join(homedir, 'Desktop', 'tsg.txt'), 'r')
     lines = f.readlines()
     lines = map(lambda line: line.strip(), lines)
     lines = map(lambda line: ''.join(filter(lambda x: x in string.printable, line)), lines)
-    getDayAndTimeFromLine(lines[1])
+    print getDayAndTimeFromLine(lines[10005])
+    print getPersonFromLine(lines[10005])
+    print getMessageFromLine(lines[10005])
     f.close()
 
 def getDayAndTimeFromLine(line):
-    print line[1]
+    return line[1:11], line[12:20]
+
+def getPersonFromLine(line):
+    return line[22:line.find(':', 22, line.__len__())]
+
+def getMessageFromLine(line):
+    return line[line.find(':', 22, line.__len__())+2:]
 
 if __name__ == '__main__':
     main()
