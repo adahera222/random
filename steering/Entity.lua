e9c269cef4ff9111e199b905ee426f2d613299af
Felipe Gonzalez <adonaac@gmail.com>
2012-11-11 22:19:20 -0200

    Add steering: seek and flee

---
+++ steering/Entity.lua
@@ -0,0 +1,67 @@
+require 'Vector'
+
+Entity = {}
+Entity.__index = Entity
+
+function Entity.new(w, h, p, v, mass, max_v, max_f)
+    return setmetatable({
+        position = p or Vector(0, 0), 
+        velocity = v or Vector(0, 0),
+        desired_velocity = Vector(0, 0),
+        mass = mass,
+        acceleration = Vector(0, 0),
+        steering = Vector(0, 0),
+        max_velocity = max_v or 0,
+        max_force = max_f or 0,
+        width = w, height = h
+    }, Entity)
+end
+
+-- target: Vector
+function Entity:update(dt, target)
+    self:seek(target)
+
+    -- Base
+    local steering_force = self.steering:min(self.max_force)
+    self.acceleration = steering_force/self.mass
+    self.velocity = (self.velocity + self.acceleration*dt):min(self.max_velocity)
+    self.position = self.position + self.velocity*dt
+end
+
+function Entity:seek(target)
+    self.desired_velocity = (target - self.position):normalized()*self.max_velocity
+    self.steering = self.desired_velocity - self.velocity
+end
+
+function Entity:flee(target)
+    self.desired_velocity = (self.position - target):normalized()*self.max_velocity
+    self.steering = self.desired_velocity - self.velocity
+end
+
+function Entity:draw()
+
+    -- Draw entity
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.rectangle('fill', 
+    self.position.x - self.width/2, self.position.y - self.height/2, 
+    self.width, self.height)
+
+    -- Draw velocity vector
+    love.graphics.setColor(0, 255, 0)
+    love.graphics.line(self.position.x, self.position.y,
+    self.position.x + self.velocity.x, self.position.y + self.velocity.y)
+
+    -- Draw desired velocity vector
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.line(self.position.x, self.position.y,
+    self.position.x + self.desired_velocity.x, self.position.y + self.desired_velocity.y)
+
+    -- Draw steering vector
+    love.graphics.setColor(0, 0, 255)
+    love.graphics.line(self.position.x, self.position.y,
+    self.position.x + self.steering.x, self.position.y + self.steering.y)
+
+    love.graphics.setColor(0, 0, 0)
+end
+
+setmetatable(Entity, {__call = function(_, ...) return Entity.new(...) end})
---
+++ steering/main.lua
@@ -0,0 +1,27 @@
+require 'Entity'
+
+function love.load()
+    love.graphics.setBackgroundColor(255, 255, 255)
+    love.graphics.setColor(0, 0, 0)
+
+    seeker1 = Entity(20, 20, Vector(400, 300), Vector(0, 0), 0.5, 300, 100)
+end
+
+function love.update(dt)
+    x, y = love.mouse.getPosition()
+
+    seeker1:update(dt, Vector(x, y))
+end
+
+function love.draw()
+
+    -- Draw mouse position
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.circle('fill', x, y, 10, 360)
+
+    seeker1:draw()
+end
+
+function love.keypressed(key)
+    if key == 'q' then love.event.push('quit') end
+end
