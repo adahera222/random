50ee895f83b46f53c20bcccdcd783b3b15250cfe
Felipe Gonzalez <adonaac@gmail.com>
2012-11-26 19:25:30 -0200

    Adding pursue and evade

--- steering/lua/Entity.lua
+++ steering/lua/Entity.lua
@@ -1,9 +1,11 @@
 require 'Vector'
+require 'utils'
 
 Entity = {}
 Entity.__index = Entity
 
-function Entity.new(w, h, p, v, mass, max_v, max_f)
+-- An entity probably shouldn't have this many fields... ;_;
+function Entity.new(w, h, p, v, mass, max_v, max_f, flee_r, arrival_r)
     return setmetatable({
         position = p or Vector(0, 0),
         velocity = v or Vector(0, 0),
@@ -15,12 +17,23 @@ function Entity.new(w, h, p, v, mass, max_v, max_f)
         max_velocity = max_v or 0,
         max_force = max_f or 0,
         width = w,
-        height = h
+        height = h,
+        behavior = 'seek',
+        seek_flee_arrival_target = nil,
+        flee_radius = flee_r,
+        arrival_radius = arrival_r,
+        pursue_evade_entity = nil,
+        pursue_evade_future_entity_position = nil
     }, Entity)
 end
 
-function Entity:update(dt, target)
-    self[current.behavior](self, target, current.radius)
+function Entity:update(dt)
+    if equalsAny(self.behavior, 'seek', 'flee', 'arrival') then
+        self[self.behavior](self, self.seek_flee_arrival_target, current.radius)
+
+    elseif equalsAny(current.behavior, 'pursue', 'evade') then
+        self[self.behavior](self, self.pursue_evade_entity)
+    end
 
     -- Base
     self.steering_force = self.steering:min(self.max_force)
@@ -29,11 +42,13 @@ function Entity:update(dt, target)
     self.position = self.position + self.velocity*dt
 end
 
+-- target: Vector
 function Entity:seek(target)
     self.desired_velocity = (target - self.position):normalized()*self.max_velocity
     self.steering = self.desired_velocity - self.velocity
 end
 
+-- target: Vector
 function Entity:flee(target, fleeRadius)
     self.desired_velocity = (self.position - target)
     local distance = self.desired_velocity:len()
@@ -51,6 +66,7 @@ function Entity:flee(target, fleeRadius)
     self.steering = self.desired_velocity - self.velocity
 end
 
+-- target: Vector
 function Entity:arrival(target, arrivalRadius)
     self.desired_velocity = (target - self.position)
     local distance = self.desired_velocity:len()
@@ -68,43 +84,67 @@ function Entity:arrival(target, arrivalRadius)
     self.steering = self.desired_velocity - self.velocity
 end
 
-function Entity:draw()
+-- target: Entity
+function Entity:pursue(target)
+    local t = 3
+    self.pursue_evade_future_entity_position = target.position + target.velocity*t 
+    self:seek(self.pursue_evade_future_entity_position)
+end
 
-    -- Draw entity
+local function drawEntity(entity, borderColor, fillColor)
     love.graphics.push()
-    love.graphics.translate(self.position.x, self.position.y)
-    love.graphics.rotate(self.velocity:angle())
-    love.graphics.translate(-self.position.x, -self.position.y)
+    love.graphics.translate(entity.position.x, entity.position.y)
+    love.graphics.rotate(entity.velocity:angle())
+    love.graphics.translate(-entity.position.x, -entity.position.y)
     love.graphics.setLineWidth(1.5)
-    love.graphics.setColor(12, 25, 50)
-    love.graphics.rectangle('line', 
-    self.position.x - self.width/2, self.position.y - self.height/2, 
-    self.width, self.height)
-    love.graphics.setColor(50, 100, 200)
-    love.graphics.rectangle('fill', 
-    self.position.x - self.width/2, self.position.y - self.height/2, 
-    self.width, self.height)
+    love.graphics.setColor(unpack(borderColor))
+    love.graphics.rectangle('line', entity.position.x - entity.width/2, 
+    entity.position.y - entity.height/2, entity.width, entity.height)
+    love.graphics.setColor(unpack(fillColor))
+    love.graphics.rectangle('fill', entity.position.x - entity.width/2, 
+    entity.position.y - entity.height/2, entity.width, entity.height)
     love.graphics.pop()
+end
+
+local function drawEntityVector(entity, vector_name, color)
+    love.graphics.setColor(unpack(color))
+    love.graphics.line(entity.position.x, entity.position.y,
+    entity.position.x + entity[vector_name].x, entity.position.y + entity[vector_name].y)
+    love.graphics.setColor(0, 0, 0)
+end
+
+local function drawFutureEntityPosition(entity, color)
+    love.graphics.setColor(unpack(color))
+    love.graphics.circle('line', 
+    entity.pursue_evade_future_entity_position.x, 
+    entity.pursue_evade_future_entity_position.y, 5, 360)
+    love.graphics.line(
+    entity.pursue_evade_future_entity_position.x - 10, 
+    entity.pursue_evade_future_entity_position.y, 
+    entity.pursue_evade_future_entity_position.x + 10, 
+    entity.pursue_evade_future_entity_position.y)
+    love.graphics.line(
+    entity.pursue_evade_future_entity_position.x, 
+    entity.pursue_evade_future_entity_position.y - 10, 
+    entity.pursue_evade_future_entity_position.x, 
+    entity.pursue_evade_future_entity_position.y + 10)
+    love.graphics.setColor(0, 0, 0)
+end
+
+function Entity:draw()
+    if equalsAny(self.behavior, 'seek', 'flee', 'arrival') then
+        drawEntity(self, {12, 25, 50}, {50, 100, 200})
+
+    elseif equalsAny(self.behavior, 'pursue', 'evade') then
+        drawEntity(self, {50, 12, 25}, {200, 50, 100})
+        drawFutureEntityPosition(self, {200, 50, 100})
+    end
 
     love.graphics.setLineWidth(1.2)
-
-    -- Draw velocity vector
-    love.graphics.setColor(0, 255, 0)
-    love.graphics.line(self.position.x, self.position.y,
-    self.position.x + self.velocity.x, self.position.y + self.velocity.y)
-
-    -- Draw desired velocity vector
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.line(self.position.x, self.position.y,
-    self.position.x + self.desired_velocity.x, self.position.y + self.desired_velocity.y)
-
-    -- Draw steering vector
-    love.graphics.setColor(255, 0, 255)
-    love.graphics.line(self.position.x, self.position.y,
-    self.position.x + self.acceleration.x, self.position.y + self.acceleration.y)
-
+    drawEntityVector(self, 'velocity', {0, 255, 0})
+    drawEntityVector(self, 'desired_velocity', {0, 0, 0})
+    drawEntityVector(self, 'acceleration', {255, 0, 255})
     love.graphics.setLineWidth(1)
-    love.graphics.setColor(0, 0, 0)
 end
 
 setmetatable(Entity, {__call = function(_, ...) return Entity.new(...) end})
--- steering/lua/main.lua
+++ steering/lua/main.lua
@@ -9,27 +9,49 @@ function love.load()
     gArrivalRadius = 200
     gFleeRadius = 100
     slowing = false
+
+    entity = Entity(20, 20, Vector(400, 300), Vector(0, 0), 1, 150, 100, gFleeRadius, gArrivalRadius)
+    pursue_evade_entity = Entity(10, 10, Vector(500, 400), Vector(0, 0), 1, 150, 100, gFleeRadius, gArrivalRadius)
+
+    -- Global behavior settings
     current = {
         behavior = 'seek',
-        radius = 200
+        radius = 200,
+        target_entity = nil
     }
-    entity = Entity(20, 20, Vector(400, 300), Vector(0, 0), 1, 150, 100)
 end
 
 function love.update(dt)
     x, y = love.mouse.getPosition()
-    entity:update(dt, Vector(x, y))
+
+    if equalsAny(current.behavior, 'seek', 'flee', 'arrival') then
+        entity.behavior = current.behavior
+        entity.seek_flee_arrival_target = Vector(x, y)
+        entity:update(dt) 
+
+    elseif equalsAny(current.behavior, 'pursue', 'evade') then
+        entity.behavior = 'seek'
+        entity.seek_flee_arrival_target = Vector(x, y) 
+        entity:update(dt)
+        pursue_evade_entity.behavior = current.behavior
+        pursue_evade_entity.pursue_evade_entity = current.target_entity 
+        pursue_evade_entity:update(dt)
+    end
 end
 
 function love.draw()
     entity:draw()
 
+    if equalsAny(current.behavior, 'pursue', 'evade') then
+        pursue_evade_entity:draw()
+    end
+
     -- Draw mouse position
     love.graphics.setColor(0, 0, 0)
-    love.graphics.circle('fill', x, y, 10, 360)
+    love.graphics.circle('line', x, y, 5, 360)
 
-    if current.behavior == 'arrival' or current.behavior == 'flee' then
-        -- Draw slowing radius
+    if equalsAny(current.behavior, 'flee', 'arrival') then
+        -- Draw slowing radius for arrival or flee
         if slowing then love.graphics.setColor(255, 0, 0)
         else love.graphics.setColor(0, 0, 0) end
         love.graphics.circle('line', x, y, current.radius, 360)
@@ -42,6 +64,7 @@ end
 function love.keypressed(key)
     if key == 'q' then love.event.push('quit') end
     if key == '1' then current.behavior = 'seek' end
-    if key == '2' then current.behavior = 'flee' ;current.radius = gFleeRadius end
+    if key == '2' then current.behavior = 'flee'; current.radius = gFleeRadius end
     if key == '3' then current.behavior = 'arrival'; current.radius = gArrivalRadius end
+    if key == '4' then current.behavior = 'pursue'; current.target_entity = entity end
 end
