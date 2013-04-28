bb8f11a2225288c6a48ecb69f2e79a13d145706e
Felipe Gonzalez <adonaac@gmail.com>
2013-04-28 01:50:06 -0300

    Upgrade UI basics

--- ld26/attacks.lua
+++ ld26/attacks.lua
@@ -1,9 +1,9 @@
 local attack = struct('activation', 'cooldown', 'damage', 'multiple', 'pierce', 'reflect', 'back', 'area')
 attacks = {}
-attacks['test'] = Attack(attack('press', 0.5, 10, 1, 1, 0, false, 'test'))
+attacks['test'] = Attack(attack('hold', 0.3, 10, 1, 0, 0, false, false)) 
 
 local area = struct('r_i', 'r_f', 'duration', 'tween', 'on_hit', 'cooldown', 'damage', 'slow')
 areas = {}
-areas['test'] = area(48, 48, 1, false, false, 0.5, 25, 0.25)
+areas['test'] = area(48, 48, 1, false, false, false, false, false)
 
 -- Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.
--- ld26/main.lua
+++ ld26/main.lua
@@ -1,18 +1,23 @@
-
 function love.load()
+    reload()
+end
+
+function reload()
     UID = 0 function getID() UID = UID + 1; return UID end
     function copy(t) local copy = {} for k, v in pairs(t) do copy[k] = v end return copy end
     physics_meter = 16
     game_over = false 
     game_paused = false
+    game_ui = false
     enemy_counter = 30
     enemies_killed = 0
     score = 0
     current_weapon_level = 0
+    current_attack_table = {} 
+    current_attack_string = nil
 
     font12 = love.graphics.newFont('res/Fipps-Regular.otf', 12)
     font16 = love.graphics.newFont('res/Fipps-Regular.otf', 16)
-    font16u = love.graphics.newFont('res/UpheavalPro.ttf', 16)
     font20 = love.graphics.newFont('res/Fipps-Regular.otf', 20)
     font24 = love.graphics.newFont('res/Fipps-Regular.otf', 24)
     font28 = love.graphics.newFont('res/Fipps-Regular.otf', 28)
@@ -27,6 +32,7 @@ function love.load()
     win_help = love.graphics.newImage('res/win_help.png')
     win2_help = love.graphics.newImage('res/win2_help.png')
     attack_help = love.graphics.newImage('res/attack_help.png')
+    upgrade_help = love.graphics.newImage('res/upgrade_help.png')
 
     require 'lib/middleclass/middleclass'
     require 'lib/chrono/chrono'
@@ -62,6 +68,12 @@ function love.draw()
     level:draw()
     camera:detach()
     
+    if game_paused then
+        love.graphics.setColor(64, 64, 64, 192)
+        love.graphics.rectangle('fill', 0, 0, 1024, 872)
+        love.graphics.setColor(255, 255, 255, 255)
+    end
+
     love.graphics.setFont(font96)
     love.graphics.setColor(0, 0, 0)
     if not game_over then 
@@ -75,10 +87,7 @@ function love.draw()
         local weapon_modifiers_text = buildTextFromAttack(level.player.attack)
         local w = font16:getWidth(weapon_modifiers_text)
         love.graphics.print(weapon_modifiers_text, 8, 832)
-    end
-
-    if not game_over and not game_paused then
-        love.graphics.print('Press ESC for help!', 8, 0) 
+        love.graphics.print('Press ESC for help!', 764, 832) 
     end
 
     if game_over then
@@ -88,11 +97,36 @@ function love.draw()
     end
 
     if game_paused then
-        love.graphics.draw(score_help, -24, 152)
-        love.graphics.draw(weapon_help, 680, 160)
-        love.graphics.draw(win_help, 616, 736)
-        love.graphics.draw(win2_help, 254, 392)
-        love.graphics.draw(attack_help, -32, 792)
+        if not game_ui then
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.draw(score_help, -24, 152)
+            love.graphics.draw(weapon_help, 680, 160)
+            love.graphics.draw(win_help, 616, 736)
+            love.graphics.draw(win2_help, 254, 392)
+            love.graphics.draw(attack_help, -32, 792)
+        end
+    end
+
+    if game_ui then
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.draw(upgrade_help, 64, 16)
+        love.graphics.setFont(font24)
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.print('Damage', 168, 128)
+        love.graphics.print('Cooldown-', 416, 128)
+        love.graphics.print('cooldowN+', 660, 128)
+        love.graphics.print('Multiple', 168, 260)
+        love.graphics.print('Speed-', 452, 260)
+        love.graphics.print('speeD+', 720, 260)
+        love.graphics.print('Pierce', 168, 392)
+        love.graphics.print('Reflecting', 416, 392)
+        love.graphics.print('Area', 760, 392)
+        love.graphics.print('area siZe', 168, 524)
+        love.graphics.print('area damaGe', 392, 524)
+        love.graphics.print('area sLow', 682, 524)
+        love.graphics.print('area damage cOoldown+', 16, 656)
+        love.graphics.print('area damage cooldoWn-', 544, 656)
+        love.graphics.print('area eXploding', 360, 788)
     end
 end
 
@@ -102,14 +136,16 @@ function buildTextFromAttack(attack)
         if k == 'multiple' then for i = 1, attack.modifiers.multiple do string = string .. 'M' end
         elseif k == 'pierce' then for i = 1, attack.modifiers.pierce do string = string .. 'P' end
         elseif k == 'reflect' then for i = 1, attack.modifiers.reflect do string = string .. 'R' end
-        elseif k == 'damage' then string = string .. 'D' .. attack.modifiers.damage
-        elseif k == 'back' then string = string .. 'B'
+        elseif k == 'damage' then if attack.modifiers.damage then string = string .. 'D' .. attack.modifiers.damage end
+        elseif k == 'back' then if attack.modifiers.back then string = string .. 'B' end
         elseif k == 'cooldown' then string = string .. 'C' .. attack.modifiers.cooldown
         elseif k == 'area' then 
-            if areas[attack.modifiers.area].cooldown then string = string .. 'AC' .. areas[attack.modifiers.area].cooldown end
-            if areas[attack.modifiers.area].damage then string = string .. 'AD' .. areas[attack.modifiers.area].damage end
-            if areas[attack.modifiers.area].slow then string = string .. 'AL' .. areas[attack.modifiers.area].slow end
-            if areas[attack.modifiers.area].on_hit then string = string .. 'AE' .. areas[attack.modifiers.area].slow end
+            if attack.modifiers.area then
+                if areas[attack.modifiers.area].cooldown then string = string .. 'AC' .. areas[attack.modifiers.area].cooldown end
+                if areas[attack.modifiers.area].damage then string = string .. 'AD' .. areas[attack.modifiers.area].damage end
+                if areas[attack.modifiers.area].slow then string = string .. 'AL' .. areas[attack.modifiers.area].slow end
+                if areas[attack.modifiers.area].on_hit then string = string .. 'AE' .. areas[attack.modifiers.area].slow end
+            end
         end
     end
     return string
@@ -118,15 +154,22 @@ end
 function love.keypressed(key)
     if key == 'q' then love.event.push('quit') end
     if key == 's' then score = score + 100 end
+    if key == 'u' then 
+        if not game_over and not game_paused then
+            game_paused = true
+            game_ui = true
+        end
+    end
     if key == 'escape' then 
         if not game_over then
             game_paused = not game_paused 
+            game_ui = false
         end
     end
     if key == 'r' then
         if game_over then
             game_over = false
-            love.load()
+            reload()
         end
     end
 
