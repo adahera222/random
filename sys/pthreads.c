275bea28091a84203a08b7aee24f32603f58f493
Felipe Gonzalez <adonaac@gmail.com>
2013-05-04 22:20:38 -0300

    ~

--- sys/pthreads.c
+++ sys/pthreads.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <string.h>
 
 #define ID(m,a,b) ((a)*(m)+(b))
 
 struct arg_struct {
-    int n, n1, m1, n2, m2;
-    int *buckets, *matrix1, *matrix2;
+    int n, line, n1, m1, n2, m2;
+    int *matrix1, *matrix2;
 };
 
 int multiplyLineColumn(int *matrix1, int *matrix2, int m1, int m2, int n, int m) {
@@ -19,9 +20,56 @@ int multiplyLineColumn(int *matrix1, int *matrix2, int m1, int m2, int n, int m)
     return result;
 }
 
-void *forkn(void *arg) {
+void *threadedMultiplyLineColumn(void *arg) {
     struct arg_struct *in = (struct arg_struct*)arg;
+    int *result = (int*)calloc(2*in->m2*in->n, sizeof(int));
+    int i, j;
+    for (i = 0; i < in->n; i++) {
+        for (j = 0; j < in->m2; j++) {
+            result[ID(in->m2,i,j)*2] = multiplyLineColumn(in->matrix1, in->matrix2, in->m1, in->m2, in->line, j);
+            result[ID(in->m2,i,j)*2+1] = in->line;
+        }
+        in->line++;
+    }
+    pthread_exit(result);
+}
+
+void printMatrix(int *matrix, int n, int m) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("%d ", matrix[ID(m,i,j)]);
+        }
+        printf("\n");
+    }
+}
+
+int* multiplyMatrix(int *matrix1, int n1, int m1, int *matrix2, int n2, int m2) {
+    int *result = (int*)calloc(n1*m2, sizeof(int));
+    if (m1 == n2) {
+        int i, j, k;
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < m2; j++) {
+                for (k = 0; k < m1; k++) {
+                    result[ID(m2,i,j)] += matrix1[ID(m1,i,k)]*matrix2[ID(m2,k,j)];
+                }
+            }
+        }
+    } else {
+        printf("Wrong dimensions.\n");
+        free(result);
+        return NULL;
+    }
+    return result;
+}
+
+int* copyArray(int *array, int size) {
+    int *result = (int*)calloc(size, sizeof(int));
     int i;
+    for (i = 0; i < size; i++) {
+        result[i] = array[i];
+    }
+    return result;
 }
 
 int main(int argc, const char *argv[]) {
@@ -60,12 +108,37 @@ int main(int argc, const char *argv[]) {
     /* printMatrix(matrix1, n1, m1); */
     /* printMatrix(matrix2, n2, m2); */
 
-    pthread_t thr1, thr2;
 
     int n = atoi(argv[3]);
     pthread_t *threads = malloc(n*sizeof(pthread_t));
+    int *buckets = calloc(n, sizeof(int));
+    j = 0;
+    for (i = 0; i < n1; i++) {
+        buckets[j] += 1;
+        j++;
+        if (j >= n) j = 0;
+    }
+
+    int *matrix12 = (int*)calloc(n1*m2, sizeof(int));
+    struct arg_struct *args = malloc(n*sizeof(struct arg_struct));
+    int **results = calloc(n, sizeof(int*));
+    int current_line = 0;
     for (i = 0; i < n; i++) {
-        pthread_create(&threads[i], NULL, forkn, NULL);
+        args[i].n1 = n1; args[i].m1 = m1; args[i].n2 = n2; args[i].m2 = m2;
+        args[i].matrix1 = copyArray(matrix1, n1*m1);
+        args[i].matrix2 = copyArray(matrix2, n2*m2);
+        args[i].n = buckets[i];
+        results[i] = calloc(2*m2*buckets[i], sizeof(int));
+        args[i].line = current_line;
+        current_line += args[i].n;
+        pthread_create(&threads[i], NULL, threadedMultiplyLineColumn, (void*)&args[i]);
+    }
+    current_line = 0;
+    for (i = 0; i < n; i++) {
+        pthread_join(threads[i], (void**)&results[i]);
+        for (j = 0; j < 2*m2*buckets[i]; j+=2) {
+            printf("%d %d\n", results[i][j], results[i][j+1]);
+        }
     }
 
     free(matrix1);
