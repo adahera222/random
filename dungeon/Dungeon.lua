fae03647c9dfcb7d986890214a740f87f65cab60
Felipe Gonzalez <adonaac@gmail.com>
2013-06-23 21:33:36 -0300

    getFreeSubgridSize

--- dungeon/Dungeon.lua
+++ dungeon/Dungeon.lua
@@ -2,15 +2,15 @@ Dungeon = {}
 Dungeon.__index = Dungeon
 
 function Dungeon.new(w, h)
-    return setmetatable({w = w or 1, h = h or 1, grid = {}}, Dungeon)
+    return setmetatable({w = w or 1, h = h or 1, grid = {}, free_slots = {}}, Dungeon)
 end
 
 function Dungeon:__tostring()
     local str = "----\n"
-    for i = 1, self.w do
+    for i = 1, self.h do
         str = str .. "[ "
-        for j = 1, self.h do
-            str = str .. self.grid[i][j] .. ", "
+        for j = 1, self.w do
+            str = str .. self.grid[i][j] .. ", \t"
         end
         str = string.sub(str, 0, -3)
         str = str .. " ]\n"
@@ -29,27 +29,39 @@ function Dungeon:initializeGrid()
     end
 end
 
-function Dungeon:getRandomRoomSize()
-    return math.random(2, 6), math.random(1, 4)
+-- Returns two random numbers between 1, 6 and 1, 4 (for room width and height)
+local function getRandomRoomSize(max_w, max_h)
+    return math.random(1, math.min(math.max(1, max_w), 6)), math.random(1, math.min(math.max(1, max_h), 4))
 end
 
-function Dungeon:getFreeSubgridWidth(i, j)
-    if self.grid[i] then
-        if self.grid[i][j] ~= 0 then return 0 end
+local function getFreeSubgridWidth(dungeon, i, j)
+    if dungeon.grid[i] then
+        if dungeon.grid[i][j] ~= 0 then return 0 end
     else return 0 end
-    return 1 + self:getFreeSubgridWidth(i+1, j)
+    return 1 + getFreeSubgridWidth(dungeon, i+1, j)
 end
 
-function Dungeon:getFreeSubgridHeight(i, j)
-    if self.grid[i] then
-        if self.grid[i][j] ~= 0 then return 0 end
+local function getFreeSubgridHeight(dungeon, i, j)
+    if dungeon.grid[i] then
+        if dungeon.grid[i][j] ~= 0 then return 0 end
     else return 0 end
-    return 1 + self:getFreeSubgridHeight(i, j+1) 
+    return 1 + getFreeSubgridHeight(dungeon, i, j+1) 
 end
 
 -- Returns w, h -> the size of the unnocupied subgrid below and to the right of i, j.
-function Dungeon:getFreeSubgridSize(i, j)
-    return self:getFreeSubgridWidth(i, j), self:getFreeSubgridHeight(i, j)
+local function getFreeSubgridSize(dungeon, i, j)
+
+end
+
+function Dungeon:findFreeSlots()
+    self.free_slots = {}
+    for i = 1, self.w do
+        for j = 1, self.h do
+            if self.grid[i][j] == 0 then 
+                table.insert(self.free_slots, {i, j})
+            end
+        end
+    end
 end
 
 -- Sets all the grids starting at position i, j and ending at i+w, j+h to id. 
@@ -67,10 +79,22 @@ function Dungeon:setSubgrid(id, i, j, w, h)
     end
 end
 
--- Adds rooms of random sizes to the grid until it's completely filled.
--- When there are few spots available it changes to filling them up with rooms of the appropriate size.
-function Dungeon:processGrid()
-    
+-- Adds rooms of random sizes to the grid until it's completely filled up :3.
+function Dungeon:generateGrid()
+    self:initializeGrid()
+    self:findFreeSlots()
+    local id = 0
+    while #self.free_slots ~= 0 do
+        id = id + 1
+        local slot = self.free_slots[math.random(1, #self.free_slots)]
+        local i, j = slot[1], slot[2]
+        local sw, sh = getFreeSubgridSize(self, i, j)
+        local w, h = getRandomRoomSize(sw, sh)
+        self:setSubgrid(id, i, j, h, w)
+        print(j, i, w, h)
+        print(self)
+        self:findFreeSlots()
+    end
 end
 
 setmetatable(Dungeon, {__call = function(_, ...) return Dungeon.new(...) end})
--- dungeon/main.lua
+++ dungeon/main.lua
@@ -6,26 +6,8 @@ math.randomseed(os.time())
 math.random(); math.random(); math.random();
 
 function love.load()
-    graph = Graph()
-    graph:addNode('1')
-    graph:addNode('2')
-    graph:addNode('3')
-    graph:addNode('4')
-    graph:addNode('5')
-    graph:addNode('6')
-    graph:addNode('7')
-    graph:addEdge('1', '3')
-    graph:addEdge('2', '3')
-    graph:addEdge('3', '4')
-    graph:addEdge('3', '6')
-    graph:addEdge('4', '5')
-    graph:addEdge('6', '7')
-    graph:addEdge('5', '7')
-    graph:floydWarshall()
-    print(graph)
-
-    dungeon = Dungeon(16, 16)
-    dungeon:initializeGrid()
+    dungeon = Dungeon(6, 6)
+    dungeon:generateGrid()
     print(dungeon)
 end
 
